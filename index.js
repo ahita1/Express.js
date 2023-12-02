f4ea50e8152e21a8d0594c0e3fc5ce011d19762d
ahita1 <ahitafani833@gmail.com>
2023-12-02 19:58:23 +0300

    adding courses route into separate routes folder

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const config = require('config')
 const express = require('express')
 const helmet = require('helmet')
+const courses = require('./routes/courses')
 const morgan = require('morgan')
 const log = require('./middleware/logger')
 const Joi = require('joi')
@@ -17,6 +18,7 @@ app.set('views', './views')          //default haha
 app.use(express.json())
 app.use(express.static('public'))
 app.use(helmet())
+app.use('/api/courses' , courses)
 
 console.log("Application Name: " + config.get('name'));
 console.log("Mail Server : " + config.get('mail.host'));
@@ -31,66 +33,10 @@ dbDebugger('connected to the database...')
 app.use(log)
 
 // const port = 3000
-const courses = [
-    { id: 1, name: 'C++' },
-    { id: 2, name: 'JavaScript' },
-    { id: 3, name: 'Python' },
-]
+
 app.get('/', (req, res) => {
     res.render('index', { title: 'My Express App', message: 'Hello Ahita haha' });
 });
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses)
-})
-
-app.post('/api/courses', (req, res) => {
-    const { error } = validate(req.body)
-    if (error)
-        return res.status(400).send(error.details[0].message);
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course)
-    res.send(course)
-})
-
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query)
-})
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(u => u.id === parseInt(req.params.id))
-    if (!course)
-        return res.status(404).send("a course with this id was not found haha")
-    res.send(course)
-})
-app.put('/api/courses/:id', (req, res) => {
-
-    const course = courses.find(u => u.id === parseInt(req.params.id))
-    if (!course)
-        return res.status(404).send("a course with the given is was not found!!!")
-
-    const { error } = validate(req.body)
-    if (error)
-        return res.status(400).send(error.details[0].message)
-    course.name = req.body.name
-    res.send(course)
-})
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find((u) => u.id === parseInt(req.params.id))
-    if (!course)
-        return res.status(404).send('A Course With The Given Id Was Not Found haha!!!')
-    const index = courses.indexOf(course)
-  courses.splice(index, 1)
-  res.send(course)
-})
-const validate = (course) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
-    return schema.validate(course)
-}
 const port  = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`server is listening on port ${port}...`);
---
+++ routes/courses.js
@@ -0,0 +1,64 @@
+import { Router } from 'express'
+const router = Router()
+
+
+const courses = [
+    { id: 1, name: 'C++' },
+    { id: 2, name: 'JavaScript' },
+    { id: 3, name: 'Python' },
+]
+
+router.get('', (req, res) => {
+    res.send(courses)
+})
+
+router.post('/', (req, res) => {
+    const { error } = validate(req.body)
+    if (error)
+        return res.status(400).send(error.details[0].message);
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
+// router.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.query)
+// })
+router.get('/:id', (req, res) => {
+    const course = courses.find(u => u.id === parseInt(req.params.id))
+    if (!course)
+        return res.status(404).send("a course with this id was not found haha")
+    res.send(course)
+})
+router.put('/:id', (req, res) => {
+
+    const course = courses.find(u => u.id === parseInt(req.params.id))
+    if (!course)
+        return res.status(404).send("a course with the given is was not found!!!")
+
+    const { error } = validate(req.body)
+    if (error)
+        return res.status(400).send(error.details[0].message)
+    course.name = req.body.name
+    res.send(course)
+})
+router.delete('/:id', (req, res) => {
+    const course = courses.find((u) => u.id === parseInt(req.params.id))
+    if (!course)
+        return res.status(404).send('A Course With The Given Id Was Not Found haha!!!')
+    const index = courses.indexOf(course)
+  courses.splice(index, 1)
+  res.send(course)
+})
+const validate = (course) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    return schema.validate(course)
+}
+
+
+export default router
